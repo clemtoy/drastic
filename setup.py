d53013d5905c4560dccc662505ef45186771d519
Clément Michard <cmich@hotmail.fr>
2017-03-08 22:25:42 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'drastic',
+  packages = ['drastic'],
+  version = '1.0',
+  description = 'Reduce the size of your Python code and increase its robustness.',
+  author = 'Clement Michard',
+  author_email = 'clement.michard@gmail.com',
+  url = 'https://github.com/clemtoy/drastic',
+  download_url = 'https://github.com/clemtoy/drastic/archive/1.0.tar.gz',
+  keywords = ['type', 'check', 'type checking', 'strict', 'annotations', 'initialization'],
+  classifiers = [],
+)
